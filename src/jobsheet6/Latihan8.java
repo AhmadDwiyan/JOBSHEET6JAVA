04c0f0b54cecea5b87d4e23b8d3c3301f8eec951
AhmadDwiyan <41880033+AhmadDwiyan@users.noreply.github.com>
2018-08-02 18:37:58 +0700

    Add files via upload

--- src/jobsheet6/Latihan8.java
+++ src/jobsheet6/Latihan8.java
@@ -10,6 +10,20 @@ import java.io.*;
 public class Latihan8 {
     public static void main(String[] args){
     BufferedReader data = new BufferedReader(new InputStreamReader(System.in)) ;
+    String x="";
+    int luas=0;
+    System.out.println("---Luas Persegi---");
+      System.out.println("Masukan Angka");
+      try {
+          x = data.readLine();
+          int angka = Integer.parseInt(x);
+          luas = angka*angka;
+      } catch (Exception e) {
+          
+          System.out.println("ERROR");
+          
+      }
+      System.out.println("HASILNYA ADALAh"+luas);
     
 }
 }
